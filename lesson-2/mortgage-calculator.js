29a3bbbf1c02d7466728669a89b9efb3b9df62ec
EC2 Default User <ec2-user@ip-172-31-27-47.us-east-2.compute.internal>
2021-02-19 15:19:09 +0000

    Updated mortgage calculator

--- lesson-2/mortgage-calculator.js
+++ lesson-2/mortgage-calculator.js
@@ -1,9 +1,13 @@
 // Input loan amount, loan duration and Annual Percentage Rate
 // Ouput monthly interest rate, loan duration(months)
 
-// Accept loan amount as integer or floats, rounding to two decimal places if necessary
-// Accept loan duration as number of months or years. Convert to months if not already
-// Accept APR as a number between 0 and 100
+/*
+Accept loan amount as integer or float,
+round to two decimal places if necessary.
+Accept loan duration as number of months or years.
+Convert to months if not already
+Accept APR as a number between 0 and 100
+*/
 
 // Convert APR to monthly interest rate
 
@@ -38,18 +42,21 @@ while (Number.isNaN(loanDuration) || loanDuration <= 0) {
 
 // Months or Years?
 console.log("Is the loan duration in months or years?\nEnter 'm' or 'y': ");
-let monthsOrYears = rl.prompt();
+let monthsOrYears = rl.prompt().toLowerCase();
 let durationInMonths;
 
-// Require months or years to be entered and convert result to months
-if (monthsOrYears === "Y".toLowerCase()) {
-  durationInMonths = loanDuration * 12;
-} else if (monthsOrYears === "M".toLowerCase()) {
-  durationInMonths = loanDuration;
-} else {
+// Require months or years to be entered
+while (!(monthsOrYears === "m" || monthsOrYears === "y")) {
   console.log("Please enter 'm' or 'y': ");
-  monthsOrYears = rl.prompt();
+  monthsOrYears = rl.prompt().toLowerCase();
 }
+
+// Convert result to months if needed
+if (monthsOrYears === "y") {
+  durationInMonths = loanDuration * 12;
+} else {
+  durationInMonths = loanDuration;
+};
 
 // APR
 console.log("Please enter an annual APR greater than zero and less than one hundred: ");
@@ -64,5 +71,6 @@ let aprAsDecimal = aprAmount / 100;
 let monthlyInterestRate = aprAsDecimal / 12;
 
 // Calculate monthly payment
-let monthlyPayment = loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-durationInMonths))));
+let monthlyPayment = loanAmount * (monthlyInterestRate / (1 - Math.pow(
+  (1 + monthlyInterestRate), (-durationInMonths))));
 console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
